17d2b4de907049b252091ef4e92bd8602d8e23b8
hellengineer97 <hellengineer97@gmail.com>
2026-05-20 11:42:03 +0300

    код достает координаты отвертий из drill

--- pygerber_test.py
+++ pygerber_test.py
@@ -1,28 +1,32 @@
 from pygerber.gerber.api import GerberFile
-from constant import BOTTOM_GBR_PATH, BOTTOM_SVG_PATH, TOP_GBR_PATH, DRILLS_GBR_PATH
-# 1. Загружаем Gerber-файл платы
+from pygerber.vm.shapely import ShapelyVirtualMachine
+from constant import BOTTOM_GBR_PATH, TOP_GBR_PATH, DRILL_GBR_PATH
+
+
+class GetDrillVirtualMachine(ShapelyVirtualMachine):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Список будет жить внутри конкретного экземпляра машины
+        self.drill_points = []
+    def on_paste_layer_eager(self, command):
+        # Ловим D03, забираем чистый вектор центра
+        self.drill_points.append((command.center.x, command.center.y))
+       
+
+def get_drill_points(gerberfile: GerberFile) -> list:    
+    rvmc = gerberfile._get_rvmc()
+    get_drill_vm = GetDrillVirtualMachine()
+    get_drill_vm.run(rvmc)
+    return get_drill_vm.drill_points
+
 gerber_bot = GerberFile.from_file(BOTTOM_GBR_PATH)
 gerber_top = GerberFile.from_file(TOP_GBR_PATH)
-gerber_drills = GerberFile.from_file(DRILLS_GBR_PATH)
-# 2. Рендерим файл в геометрию (получаем объект ShapelyImage)
+gerber_drill = GerberFile.from_file(DRILL_GBR_PATH)
+
 shapely_image_bot = gerber_bot.render_with_shapely()
 shapely_image_top = gerber_top.render_with_shapely()
-shapely_image_drills = gerber_drills.render_with_shapely()
-# 3. Напрямую сохраняем результат в SVG-файл
-# shapely_image.save_svg(BOTTOM_SVG_PATH)
+drill_points = get_drill_points(gerber_drill)
 
-# print(f"Векторный файл успешно сохранен в: {BOTTOM_SVG_PATH}")
-from shapely.geometry import MultiPolygon, GeometryCollection
-
-main_shape = shapely_image_drills._result.shape
-
-centers = []
-
-for geom in main_shape.geoms:
-  # Для каждого отдельного отверстия берем центр
-    c = geom.centroid
-    centers.append((c.x, c.y))
-
-print(f"Извлечено центров: {len(centers)}")
-for i, (x, y) in enumerate(centers):
-    print(f"Центр {i+1}: X={x:.4f}, Y={y:.4f}")
+print(f"\nНайдено отверстий (D03): {len(drill_points)}")
+for i, (x, y) in enumerate(drill_points, 1):
+    print(f"#{i}: X={x}, Y={y}")
