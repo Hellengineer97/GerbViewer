f8c24794b377bdbabc774b5da5dc203fb9475f50
hellengineer97 <hellengineer97@gmail.com>
2026-05-26 17:24:31 +0300

    прогрмамма умеет выводить пересечения отвертий и полигонов

--- pygerber_test.py
+++ pygerber_test.py
@@ -1,10 +1,12 @@
 from pygerber.gerber.api import GerberFile
 from pygerber.vm.shapely import ShapelyVirtualMachine
 from shapely.geometry.polygon import Polygon
+from shapely.geometry import Point
 from constant import BOTTOM_GBR_PATH, TOP_GBR_PATH, DRILL_GBR_PATH, TOP_SVG_PATH, BOTTOM_SVG_PATH
 
 
 class GetDrillVirtualMachine(ShapelyVirtualMachine):
+    """Переделанная вирт машина чтобы вытягивать координаты отвертий"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Список будет жить внутри конкретного экземпляра машины
@@ -13,7 +15,8 @@ class GetDrillVirtualMachine(ShapelyVirtualMachine):
         # Ловим D03, забираем чистый вектор центра
         self.drill_points.append((command.center.x, command.center.y))
        
-class CustomPolygon:
+class CustomPolygon():
+    """Переделанный класс полигона который генерит SVG элемент без лишнего мусора"""
     def __init__(self, poly_object):
         self._poly = poly_object
     def __getattr__(self, name):
@@ -35,7 +38,7 @@ class CustomPolygon:
                 path_segments.append(f"M {coords[0]} L {' '.join(coords[1:])} Z")
                 
         path = " ".join(path_segments)
-        return f'<path d="{path}" />'
+        return f'<path tabindex="0" d="{path}" />'
 
 
 def get_drill_points(gerberfile: GerberFile) -> list:    
@@ -59,10 +62,22 @@ def print_multipol(gerberfile: GerberFile):
     rvmc = gerberfile._get_rvmc()
     shvm = ShapelyVirtualMachine()
     shapely_result = shvm.run(rvmc)
-    geom = CustomPolygon(shapely_result.shape.geoms[0])
-    print(geom.svg())
-    print(type(geom)) 
-    #print(len(geoms))
+    custom_polygons = [CustomPolygon(geom) for geom in shapely_result.shape.geoms]
+    print(custom_polygons[0].svg())
+
+def print_drill_intersects(gerberfile: GerberFile, drill: GerberFile):
+    rvmc = gerberfile._get_rvmc()
+    shvm = ShapelyVirtualMachine()
+    shapely_result = shvm.run(rvmc)
+    polygons = shapely_result.shape.geoms
+    drills = get_drill_points(drill)
+    for i, polygon in enumerate(polygons):
+        for ii, point in enumerate(drills):
+            point = Point(point)
+            if polygon.intersects(point):
+                print(f"Точка {ii} попала в полигон {i}")
+
+
 
 gerber_bot = GerberFile.from_file(BOTTOM_GBR_PATH)
 gerber_top = GerberFile.from_file(TOP_GBR_PATH)
@@ -75,8 +90,7 @@ drill_points = get_drill_points(gerber_drill)
 shapely_image_top.save_svg(TOP_SVG_PATH)
 shapely_image_bot.save_svg(BOTTOM_SVG_PATH)
 
-print_multipol(gerber_top)
-
-
+print_drill_intersects(gerber_top, gerber_drill)
+print_drill_intersects(gerber_bot, gerber_drill)
 
 
